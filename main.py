378a06dcc9f945a2cc6f4ba5bec6489473410979
Psychol0gicl <sam.kokoska@hotmail.cz>
2025-07-28 20:46:40 +0200

    New todo's and issues'

--- main.py
+++ main.py
@@ -22,3 +22,7 @@ check_and_setup_entropy_cache(game.all_words, game.common_words)
 results = game.test_bot_optimized_x_words(game.common_words,x=100,  bot_class=CachedEntropyWordleBot , file_name= "cached_entropy_bot_common_words.txt")
 results = game.test_bot_optimized_x_words(game.common_words,x=100,  bot_class=NonGreedyCachedEntropyWordleBot, file_name= "non_greedy_cached_entropy_bot_common_words.txt")
 print("Testing bots completed.")
+
+
+# TODO: #4 Make non-greedy bot faster   
+# TODO: #5 Fix random seed initialization 
