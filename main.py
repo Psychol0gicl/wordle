95027d018755decb65d46179914dadc8c6e57a1e
Psychol0gicl <sam.kokoska@hotmail.cz>
2025-07-30 18:01:19 +0200

    Testing printout of progress .

--- main.py
+++ main.py
@@ -23,19 +23,29 @@ check_and_setup_letter_frequency_cache(game.all_words)
 # results = game.test_bot_optimized_x_words(game.common_words,x=100, bot_class=CachedEntropyWordleBot , file_name= "cached_entropy_bot_common_words.txt")
 
 # results = game.test_bot_optimized_x_words(game.common_words,x=100,  bot_class=CachedEntropyWordleBot , file_name= "cached_entropy_bot_common_words.txt")
-results = game.test_bot_optimized_x_words(game.common_words,x=100,  bot_class=NonGreedyCachedEntropyWordleBot, file_name= "non_greedy_cached_entropy_bot_common_words.txt")
+# results = game.test_bot_optimized_x_words(game.common_words,x=100,  bot_class=NonGreedyCachedEntropyWordleBot, file_name= "non_greedy_cached_entropy_bot_common_words.txt")
 
 
 def test_all_words_on_all_bots():
+    print(f"Testing bots on {len(game.common_words)} words. This will take a while (probably). Please wait.")
+    print(f"Results will be saved into separate for each word, progress will be written into the console.")
     bot_classes = [WordleBot, EntropyWordleBot, FastEntropyWordleBot, CachedEntropyWordleBot, NonGreedyCachedEntropyWordleBot]
-    bot_classes = [NonGreedyCachedEntropyWordleBot]
     words = game.common_words
+    i = 0
 
     for word in words:
-        # game.test_every_bot_on_a_single_word(word, bot_classes, file_name="every_bot_on_a_single_word.txt")
-        game.test_every_bot_on_a_single_word(word, bot_classes)
+        if i % 300 == 1:
+            tmp = sys.stdout
+            sys.stdout = sys.__stdout__
+            print(f"Progress: {i+1}/{len(words)}")
+            sys.stdout = tmp    
+            
 
-# test_all_words_on_all_bots()
+        i += 1
+        game.test_every_bot_on_a_single_word(word, bot_classes, file_name="every_bot_on_a_single_word.txt")
+        # game.test_every_bot_on_a_single_word(word, bot_classes)
+
+test_all_words_on_all_bots()
 
 sys.stdout = sys.__stdout__
 print("Testing bots completed.")
